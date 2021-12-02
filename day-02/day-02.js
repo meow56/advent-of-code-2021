b10ccb1bc33d3aeb46f3a38b44a4b0588e104767
meow56 <spinattacktotheface@gmail.com>
2021-12-01 23:14:10 -0600

    Day 02-2
    Just as easy.

--- day-02/day-02.js
+++ day-02/day-02.js
@@ -29,4 +29,27 @@ function day2(input) {
 	displayText(`Horizontal: ${dist}`);
 	displayText(`Depth:      ${depth}`);
 	displayText(`Mult:       ${dist * depth}`);
+
+	dist = 0; // still modified by forward
+	depth = 0; // now also modified by forward
+	let aim = 0; // modified by down and up
+
+	inst.forEach(function(elem) {
+		switch(elem[0]) {
+			case "forward":
+				dist += elem[1];
+				depth += elem[1] * aim;
+				break;
+			case "down":
+				aim += elem[1];
+				break;
+			case "up":
+				aim -= elem[1];
+				break;
+		}
+		if(depth < 0) throw "Flying submarine";
+	});
+	displayText(`True horizontal: ${dist}`);
+	displayText(`True depth:      ${depth}`);
+	displayText(`True mult:       ${dist * depth}`);
 }
