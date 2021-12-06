6b9a91412897a67d4aa1151ced28a0d9a9701d32
meow56 <spinattacktotheface@gmail.com>
2021-12-06 10:05:48 -0600

    Day 06-2
    Well, I guess it was easier said than done, huh. The previous solution was too intensive, so I had to transition to considering buckets of fish instead of each fish individually. Still much shorter than I thought, honestly.

--- day-06/day-06.js
+++ day-06/day-06.js
@@ -9,18 +9,57 @@ function day6(input) {
 	}
 	ages = ages.filter(e => e.length === 1).map(e => +e);
 	
-	const DAYS = 80;
-	for(let i = 1; i <= DAYS; i++) {
-		let toAdd = [];
-		ages = ages.map(function(fish) {
-			if(--fish === -1) {
-				fish = 6;
-				toAdd.push(8);
-			}
-			return fish;
-		});
-		ages = ages.concat(toAdd);
-		//console.log(`Day ${i}, ${ages.length} fish: ${ages}`);
+	// Oh no, it turns out simulating 256 days is too intensive.
+	// Can't believe simulating ~1e11 (2^(256/7)) fish is too much.
+	let age_buckets = new Map();
+	age_buckets.set(0, 0);
+	age_buckets.set(1, 0);
+	age_buckets.set(2, 0);
+	age_buckets.set(3, 0);
+	age_buckets.set(4, 0);
+	age_buckets.set(5, 0);
+	age_buckets.set(6, 0);
+	for(let i = 0; i < ages.length; i++) {
+		age_buckets.set(ages[i], age_buckets.get(ages[i]) + 1);
 	}
-	displayText(`On day ${DAYS}, there are ${ages.length} fish.`);
+	age_buckets.set(7, 0);
+	age_buckets.set(8, 0);
+
+	function getLength() {
+		return age_buckets.get(0) +
+			   age_buckets.get(1) +
+			   age_buckets.get(2) +
+			   age_buckets.get(3) +
+			   age_buckets.get(4) +
+			   age_buckets.get(5) +
+			   age_buckets.get(6) +
+			   age_buckets.get(7) +
+			   age_buckets.get(8);
+	}
+
+	const DAYS = 256;
+	for(let i = 1; i <= DAYS; i++) {
+		age_buckets.forEach(function(bucket, age, map) {
+			if(age !== -1) {
+				map.set(age - 1, bucket);
+			}
+		});
+		age_buckets.set(6, age_buckets.get(-1) + age_buckets.get(6));
+		age_buckets.set(8, age_buckets.get(-1));
+		if(i === 80) {
+			displayText(`On day 80, there are ${getLength()} fish.`);
+		}
+		console.groupCollapsed(`Day ${i}: ${getLength()} fish`);
+		console.log(`0 fish: ${age_buckets.get(0)}`);
+		console.log(`1 fish: ${age_buckets.get(1)}`);
+		console.log(`2 fish: ${age_buckets.get(2)}`);
+		console.log(`3 fish: ${age_buckets.get(3)}`);
+		console.log(`4 fish: ${age_buckets.get(4)}`);
+		console.log(`5 fish: ${age_buckets.get(5)}`);
+		console.log(`6 fish: ${age_buckets.get(6)}`);
+		console.log(`7 fish: ${age_buckets.get(7)}`);
+		console.log(`8 fish: ${age_buckets.get(8)}`);
+		console.groupEnd();
+	}
+	displayText(`On day ${DAYS}, there are ${getLength()} fish.`);
 }
