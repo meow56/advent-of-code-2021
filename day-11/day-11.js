3cc72340316b0f6eb9ea3a3cfbea98078cf8e21f
meow56 <spinattacktotheface@gmail.com>
2021-12-11 10:52:44 -0600

    Day 11-2
    Ay, it's an off-by-one error! I set i = 1, but then i++ happens, so my step counter was off.
    Also, it's animation!!! Wowowow!!!

--- day-11/day-11.js
+++ day-11/day-11.js
@@ -22,11 +22,13 @@ function day11(input) {
 		this.energy = energy;
 		this.hasFlashed = false;
 		this.neighbors = [];
+		this.cooldown = 0;
 
 		this.increase = function() {
 			this.energy++;
 			if(!this.hasFlashed && this.energy > 9) {
 				this.hasFlashed = true;
+				this.cooldown = 1;
 				this.neighbors.forEach(function(neighbor) {
 					neighbor.increase();
 				});
@@ -96,7 +98,31 @@ function day11(input) {
 	});
 
 	let totalFlash = 0;
-	for(let i = 1; i <= 100; i++) {
+	let currentTotal = 0;
+	const NUM_OCT = octopi.flat().length;
+	let i = 0;
+	let steps = [[]];
+	let step100Flash;
+	octopi.forEach(function(row) {
+		let toDisplay = "";
+		row.forEach(function(octopus) {
+			if(octopus.hasFlashed) {
+				octopus.energy = 0;
+				toDisplay += " ";
+				totalFlash++;
+				currentTotal++;
+			} else if(octopus.cooldown !== 0) {
+				toDisplay += "▒";
+				octopus.cooldown--;
+			} else {
+				toDisplay += "█";
+			}
+		});
+		steps[0].push(toDisplay);
+	});
+	while(currentTotal !== NUM_OCT) {
+		currentTotal = 0;
+		i++;
 		octopi.forEach(function(row) {
 			row.forEach(function(octopus) {
 				octopus.hasFlashed = false;
@@ -109,7 +135,7 @@ function day11(input) {
 			});
 		});
 
-		displayText(`Step ${i}`);
+		let thisStep = [];
 		octopi.forEach(function(row) {
 			let toDisplay = "";
 			row.forEach(function(octopus) {
@@ -117,15 +143,39 @@ function day11(input) {
 					octopus.energy = 0;
 					toDisplay += " ";
 					totalFlash++;
+					currentTotal++;
+				} else if(octopus.cooldown !== 0) {
+					toDisplay += "▒";
+					octopus.cooldown--;
 				} else {
 					toDisplay += "█";
 				}
 			});
-			displayText(toDisplay);
+			thisStep.push(toDisplay);
 		});
-		displayText();
+		steps.push(thisStep);
+		if(i === 100) {
+			displayText(`Total flashes: ${totalFlash}`);
+			step100Flash = totalFlash;
+		}
 	}
-	displayText(`Total flashes: ${totalFlash}`);
-	updateCaption(`100 maps are displayed, showing which octopi flashed on a given step.`);
-	updateCaption(`The total number of flashes is shown: ${totalFlash}`);
+	displayText(`Sync time: ${i}`);
+	const ANIM_INTERVAL = 1000;
+	function animate(step) {
+		if(step === steps.length) return;
+		let toAnimate = steps[step];
+		clearText();
+		displayText(`Total flashes: ${step100Flash}`);
+		displayText(`Sync time: ${i}`);
+		displayText(`Step ${step}:`);
+		toAnimate.forEach(displayText);
+		setTimeout(animate, ANIM_INTERVAL, ++step);
+	}
+	animate(0);
+	updateCaption(`An animation plays, showing the state of each octopus at each step.`);
+	updateCaption(`Octopi that were lit on the previous step are displayed with ▒.`);
+	updateCaption(`Octopi that are lit on the current step are displayed with " ".`);
+	updateCaption(`Other octopi are displayed with █.`);
+	updateCaption(`The total number of flashes is shown: ${step100Flash}`);
+	updateCaption(`The sync time is also shown: ${i}`);
 }
