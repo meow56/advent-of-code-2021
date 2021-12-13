d0c988803c43ee4aeade4e386fa325769209a418
meow56 <spinattacktotheface@gmail.com>
2021-12-13 11:31:52 -0600

    Day 13-2
    Hmhmhm, man I sure am doing a good job so far!
    ...Wait, did I flip X when I was supposed to flip Y?
    What!?
    
    Also, annoyingly the second fold is NOT exactly halfway between 0 and highestY, so I had to add a little code to fix that. I also added it to highestX just in case. And I added both cases super-just-in-case.
    
    Oh, also also. The displaying is really slow. The calculations run fast, but displaying each interim step takes a while. So if you're impatient, comment out lines 107-112.

--- day-13/day-13.js
+++ day-13/day-13.js
@@ -1,7 +1,27 @@
 "use strict";
 
 function day13(input) {
-	/*input = ``;*/
+	/*input = `6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
+
+fold along y=7
+fold along x=5`;*/
 	const FILE_REGEX = /(\d+,\d+)|(?:fold along (x|y)=(\d+))/gm;
 	let points = [];
 	let folds = [];
@@ -35,6 +55,7 @@ function day13(input) {
 	});
 
 	folds.forEach(function(fold, fI) {
+		console.time(`Deriving fold ${fI + 1}`);
 		if(fold[0] === "x") {
 			let toFold = paper.map(function(line) {
 				return line.filter((e, ind) => ind > fold[1]).reverse();
@@ -42,6 +63,16 @@ function day13(input) {
 			let unfolded = paper.map(function(line) {
 				return line.filter((e, ind) => ind < fold[1]);
 			});
+			while(toFold[0].length < unfolded[0].length) {
+				toFold = toFold.map(function(row) {
+					return row.reverse().push(".").reverse();
+				}
+			}
+			while(toFold[0].length > unfolded[0].length) {
+				unfolded = unfolded.map(function(row) {
+					return row.reverse().push(".").reverse();
+				}
+			}
 			toFold.forEach(function(row, rI) {
 				row.forEach(function(point, pI) {
 					if(point === "█") {
@@ -53,8 +84,14 @@ function day13(input) {
 		} else if(fold[0] === "y") {
 			let toFold = paper.filter((line, lI) => lI > fold[1]);
 			let unfolded = paper.filter((line, lI) => lI < fold[1]);
+			while(toFold.length < unfolded.length) {
+				toFold.push("".padStart(paper[0].length, ".").split(""));
+			}
+			while(toFold.length > unfolded.length) {
+				unfolded.reverse().push("".padStart(paper[0].length, ".").split("")).reverse();
+			}
+			toFold.reverse();
 			toFold.forEach(function(row, rI) {
-				row.reverse();
 				row.forEach(function(point, pI) {
 					if(point === "█") {
 						unfolded[rI][pI] = "█";
@@ -65,11 +102,15 @@ function day13(input) {
 		} else {
 			throw `Not folding along x or y`;
 		}
+		console.timeEnd(`Deriving fold ${fI + 1}`);
 
+		console.time(`Displaying fold ${fI + 1}`);
 		displayText(`After fold ${fI + 1}:`);
 		paper.forEach(function(row) {
 			displayText(row.join(""));
 		});
+		console.timeEnd(`Displaying fold ${fI + 1}`);
+
 		if(fI === 0) {
 			let dotCount = paper.reduce(function(acc, row) {
 				let rowSum = row.reduce(function(rAcc, point) {
@@ -78,7 +119,18 @@ function day13(input) {
 				return acc + rowSum;
 			}, 0);
 			displayText(`Visible points after first fold: ${dotCount}`);
-			throw `Getting ahead of ourselves!`;
+			updateCaption(`The number of visible points after the first fold is shown: ${dotCount}`);
 		}
 	});
+
+
+	displayText(`Final`);
+	paper.forEach(function(row) {
+		displayText(row.join(""));
+	});
+	updateCaption(`There are many grids containing . and █`);
+	updateCaption(`Every successive grid represents the paper after a fold.`);
+	updateCaption(`The last one is shown twice, which displays the letters`);
+	updateCaption(`that make up part 2's solution.`);
+	updateCaption(`Unfortunately, I don't know what it says. Sorry.`);
 }
