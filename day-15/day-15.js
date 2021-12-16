47546334660fa8f890a0994c6ffc2a54b88bf2e4
meow56 <spinattacktotheface@gmail.com>
2021-12-15 21:12:06 -0600

    Day 15-2.1
    I think this is about my limit. I implemented some basic things, like caching the neighbors and ensuring only squares whose neighbors were updated on the previous turn are updated on this turn. It's not perfect, not by a long shot--it seems to take around 15-16 seconds still, down from 30 previously. I'm sure there are some improvements that I'm missing, but for now I think I'm done with thinking about this problem.

--- day-15/day-15.js
+++ day-15/day-15.js
@@ -25,7 +25,11 @@ function day15(input) {
 		this.riskPath = new Map();
 		this.minPath = new Map();
 		this.onPath = new Map();
-		this.lowPathTo = new Map();
+		this.needsUpdate = new Map();
+		this.lNeigh = new Map();
+		this.rNeigh = new Map();
+		this.dNeigh = new Map();
+		this.uNeigh = new Map();
 
 		this.findPath = function(boundsCrossed) {
 			if(boundsCrossed[0] === "L") boundsCrossed = boundsCrossed.slice(2);
@@ -79,8 +83,23 @@ function day15(input) {
 			if(this.riskPath.has(boundsCrossed)) return this.riskPath.get(boundsCrossed);
 			let cLen = chitons.length;
 			let rowLen = chitons[0].length;
-			let rNeigh = chitons[this.posY][mod(this.posX + 1, rowLen)];
-			let dNeigh = chitons[mod(this.posY + 1, cLen)][this.posX];
+			let rNeigh, dNeigh;
+			let rWrap = !igRep && (this.posX + 1 === rowLen) ? "R" : "";
+			let dWrap = !igRep && (this.posY + 1 === cLen) ? "D" : "";
+			if(this.rNeigh.has(boundsCrossed)) {
+				rNeigh = this.rNeigh.get(boundsCrossed)[0];
+			} else {
+				rNeigh = chitons[this.posY][mod(this.posX + 1, rowLen)];
+				this.rNeigh.set(boundsCrossed, [rNeigh, rWrap]);
+				rNeigh.lNeigh.set(rWrap + boundsCrossed, [this, rWrap ? "L" : ""]);
+			}
+			if(this.dNeigh.has(boundsCrossed)) {
+				dNeigh = this.dNeigh.get(boundsCrossed)[0];
+			} else {
+				dNeigh = chitons[mod(this.posY + 1, cLen)][this.posX];
+				this.dNeigh.set(boundsCrossed, [dNeigh, dWrap]);
+				dNeigh.uNeigh.set(boundsCrossed + dWrap, [this, dWrap ? "U" : ""]);
+			}
 			let numCross = igRep ? 0 : boundsCrossed.length;
 			let numR = 0;
 			let numD = 0;
@@ -89,13 +108,13 @@ function day15(input) {
 				if(boundsCrossed[i] === "D") numD++;
 			}
 
-			let rWrap, rRisk;
+			let rRisk;
 			if((!igRep && numR !== 4) || this.posX + 1 !== rowLen) {
 				rWrap = !igRep && (this.posX + 1 === rowLen) ? "R" : "";
 				rRisk = rNeigh.calcRiskPath(rWrap + boundsCrossed, igRep) + 
 						rNeigh.getRiskOff(numCross + +!!rWrap);
 			}
-			let dWrap, dRisk;
+			let dRisk;
 			if((!igRep && numD !== 4) || this.posY + 1 !== cLen) {
 				dWrap = !igRep && (this.posY + 1 === cLen) ? "D" : "";
 				dRisk = dNeigh.calcRiskPath(boundsCrossed + dWrap, igRep) + 
@@ -127,14 +146,21 @@ function day15(input) {
 	chitons[0][0].calcRiskPath("", false);
 	// OK, it's iterative optimization time.
 	let optimized = false;
-	let iterations = 1;
+	let iters = 1;
+	console.time(`Optimization`);
 	while(!optimized) {
 		optimized = true;
 		let changes = 0;
-		let maxChanges = 0;
+		let checked = 0;
+		let queued = 0;
 		chitons.forEach(function(row) {
 			row.forEach(function(square) {
 				square.riskPath.forEach(function(risk, tile) {
+					if(iters !== 1 && square.needsUpdate.has(tile) && !square.needsUpdate.get(tile)) {
+						return;
+					}
+					checked++;
+					square.needsUpdate.set(tile, false);
 					let cLen = chitons.length;
 					let rowLen = row.length;
 					let numR = 0;
@@ -144,35 +170,32 @@ function day15(input) {
 						if(tile[i] === "D") numD++;
 					}
 					let numCross = numR + numD;
-					let rNeigh = chitons[square.posY][mod(square.posX + 1, rowLen)];
-					let dNeigh = chitons[mod(square.posY + 1, cLen)][square.posX];
-					let lNeigh = chitons[square.posY][mod(square.posX - 1, rowLen)];
-					let uNeigh = chitons[mod(square.posY - 1, cLen)][square.posX];
-					let rWrap, rRisk;
-					if(numR !== 4 || square.posX + 1 !== rowLen) {
-						rWrap = square.posX + 1 === rowLen ? "R" : "";
+					let [rNeigh, rWrap] = square.rNeigh.get(tile);
+					let [dNeigh, dWrap] = square.dNeigh.get(tile);
+					let [lNeigh, lWrap] = square.lNeigh.get(tile) || [undefined, undefined];
+					let [uNeigh, uWrap] = square.uNeigh.get(tile) || [undefined, undefined];
+
+					let rRisk;
+					if(iters !== 1 && (numR !== 4 || square.posX + 1 !== rowLen)) {
 						rRisk = rNeigh.calcRiskPath(rWrap + tile) + 
-								rNeigh.getRiskOff(numCross + +!!rWrap);
+								rNeigh.getRiskOff(numCross + +!!rWrap); // Janky!!
 					}
-					let dWrap, dRisk;
-					if(numD !== 4 || square.posY + 1 !== cLen) {
-						dWrap = square.posY + 1 === cLen ? "D" : "";
+					let dRisk;
+					if(iters !== 1 && (numD !== 4 || square.posY + 1 !== cLen)) {
 						dRisk = dNeigh.calcRiskPath(tile + dWrap) + 
 								dNeigh.getRiskOff(numCross + +!!dWrap);
 					}
-					let uWrap, uRisk;
+					let uRisk;
 					if(numD !== 0 || square.posY !== 0) {
-						uWrap = square.posY === 0 ? "U" : "";
 						uRisk = uNeigh.calcRiskPath(tile + uWrap) + 
 								uNeigh.getRiskOff(numCross + +!!uWrap);
 					}
-					let lWrap, lRisk;
+					let lRisk;
 					if(numR !== 0 || square.posX !== 0) {
-						lWrap = square.posX === 0 ? "L" : "";
 						lRisk = lNeigh.calcRiskPath(lWrap + tile) + 
-								lNeigh.getRiskOff(numCross + +!!lWrap); // Janky!
+								lNeigh.getRiskOff(numCross + +!!lWrap);
 					}
-					let lowestRisk = rRisk;
+					let lowestRisk = rRisk || 100000000000;
 					let lowestName = "R";
 					if(dRisk < lowestRisk) {
 						lowestRisk = dRisk;
@@ -191,12 +214,26 @@ function day15(input) {
 						optimized = false;
 						square.riskPath.set(tile, lowestRisk);
 						square.minPath.set(tile, lowestName);
+						if(lowestName !== "R") rNeigh.needsUpdate.set(rWrap + tile, true);
+						if(lNeigh && lowestName !== "L") {
+							lNeigh.needsUpdate.set(tile.slice(+!!lWrap), true);
+						}
+						if(lowestName !== "D") dNeigh.needsUpdate.set(tile + dWrap, true);
+						if(uNeigh && lowestName !== "U") {
+							uNeigh.needsUpdate.set(tile.slice(0, tile.length - +!!uWrap), true);
+						}
 					}
 				});
 			});
 		});
-		if(iterations++ % 10 === 0) console.log(`Iteration ${iterations - 1}; Changes: ${changes}`);
+		if(iters++ % 10 === 0) {
+			console.log(`Iteration ${iters - 1}; changes / checked: ${changes} / ${checked}`);
+		}
+		if(changes === 0 && !optimized) {
+			throw `Unoptimal, but nothing happened.`;
+		}
 	}
+	console.timeEnd(`Optimization`);
 	displayText(`Min repRisk level: ${chitons[0][0].calcRiskPath("", false)}`);
 	chitons[0][0].findPath("");
 	for(let d = 0; d < 5; d++) {
