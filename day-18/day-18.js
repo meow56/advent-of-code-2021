7c90f7797c70df2da3f66e8047d289ef39c36c24
meow56 <spinattacktotheface@gmail.com>
2021-12-19 13:55:00 -0600

    Day 18-2
    Pretty good! I had to mess around a lot because merge() changed the original tree, but it got done eventually.

--- day-18/day-18.js
+++ day-18/day-18.js
@@ -1,85 +1,23 @@
 "use strict";
 
 function day18(input) {
-	/*input = `[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]
-[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]
-[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]
-[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]
-[7,[5,[[3,8],[1,4]]]]
-[[2,[2,2]],[8,[8,1]]]
-[2,9]
-[1,[[[9,3],9],[[9,0],[0,7]]]]
-[[[5,[7,4]],7],1]
-[[[[4,2],2],6],[8,7]]`;/**/
+	/*input = `[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
+[[[5,[2,8]],4],[5,[[9,9],0]]]
+[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
+[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
+[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
+[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
+[[[[5,4],[7,7]],8],[[8,3],8]]
+[[9,3],[[9,9],[6,[4,9]]]]
+[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
+[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]`;/**/
 	const FILE_REGEX = /(?:\[|\]|\d|,)+/gm;
 	let numbers = [];
 	let entry;
 	while(entry = FILE_REGEX.exec(input)) {
 		numbers.push(entry[0]);
 	}
-	// too low: 166
-	console.log(numbers);
-
-	function decToBin(num) {
-		let bin = "";
-		while(num !== 0) {
-			if(num % 2 === 1) {
-				bin += "1";
-				num = (num - 1) / 2;
-			} else {
-				bin += "0";
-				num /= 2;
-			}
-		}
-		return bin.split("").reverse().join("");
-	}
-
-
-	function reduce(num) {
-		let finished = false;
-		while(!finished) {
-			finished = true;
-			for(let i = 0; i < num.length; i++) {
-				if(num[i][0] > 4) {
-					finished = false;
-					// Explode!
-					// num[i + 1] must be the other item in the pair.
-					if(num[i + 1][0] !== num[i][0]) {
-						throw `I thought ${num[i]} and ${num[i + 1]} were a pair, but they aren't.`;
-					}
-
-					if(i === 0) {
-						// No left number.
-						num[i + 2][1] += num[i + 1][1];
-						num.splice(i, 2, [num[i][0] - 1, 0]);
-						i--; // to realign.
-					} else if(i === num.length - 2) {
-						// No right number.
-						num[i - 1][1] += num[i][1];
-						num.splice(i, 2, [num[i][0] - 1, 0]);
-						i--; // realign again.
-						// though we're at the end of the array, so whatever.
-					} else {
-						num[i - 1][1] += num[i][1];
-						num[i + 2][1] += num[i + 1][1];
-						num.splice(i, 2, [num[i][0] - 1, 0]);
-					}
-				}
-			}
-
-			for(let i = 0; i < num.length; i++) {
-				if(num[i][1] > 9) {
-					finished = false;
-					// Split!
-					num.splice(i, 1, [num[i][0] + 1, Math.floor(num[i][1] / 2)],
-									 [num[i][0] + 1, Math.ceil(num[i][1] / 2)]);
-					break;
-					// We need to make sure any explosions happen first.
-				}
-			}
-		}
-		return num;
-	}
+	// too high: 4796
 
 	function TreeNode(id) {
 		this.id = id;
@@ -126,28 +64,24 @@ function day18(input) {
 		}
 	}
 
-	function Tree() {
-		this.nodes = [];
+	function Tree(nodes) {
+		this.nodes = nodes || [];
 
 		this.displayTree = function() {
-			/*                                                               01
-			 *                               02                                                              03
-			 *               04                              05                              06                              07
-			 *       08              09              10              11              12              13              14              15
-			 *   16      17      18      19      20      21      22      23      24      25      26      27      28      29      30      31
-			 * 32  33  34  35  36  37  38  39  40  41  42  43  44  45  46  47  48  49  50  51  52  53  54  55  56  57  58  59  60  61  62  63
-			 */
-			let noDi= [];
+			let noDi = [];
 			for(let i = 0; i < this.nodes.length; i++) {
 				noDi[i] = this.nodes[i].value || this.nodes[i].value === 0 ? this.nodes[i].value.toString().padStart(2, "0") : "__";
 			}
-			displayText(`                                                              ${noDi[0]}`);
-			displayText(`                              ${noDi[1]}                                                              ${noDi[2]}`);
-			displayText(`              ${noDi[3]}                              ${noDi[4]}                              ${noDi[5]}                              ${noDi[6]}`);
-			displayText(`      ${noDi[7]}              ${noDi[8]}              ${noDi[9]}              ${noDi[10]}              ${noDi[11]}              ${noDi[12]}              ${noDi[13]}              ${noDi[14]}`);
-			displayText(`  ${noDi[15]}      ${noDi[16]}      ${noDi[17]}      ${noDi[18]}      ${noDi[19]}      ${noDi[20]}      ${noDi[21]}      ${noDi[22]}      ${noDi[23]}      ${noDi[24]}      ${noDi[25]}      ${noDi[26]}      ${noDi[27]}      ${noDi[28]}      ${noDi[29]}      ${noDi[30]}`);
-			if(this.nodes.length > 32) {
-			displayText(`${noDi[31]}  ${noDi[32]}  ${noDi[33]}  ${noDi[34]}  ${noDi[35]}  ${noDi[36]}  ${noDi[37]}  ${noDi[38]}  ${noDi[39]}  ${noDi[40]}  ${noDi[41]}  ${noDi[42]}  ${noDi[43]}  ${noDi[44]}  ${noDi[45]}  ${noDi[46]}  ${noDi[47]}  ${noDi[48]}  ${noDi[49]}  ${noDi[50]}  ${noDi[51]}  ${noDi[52]}  ${noDi[53]}  ${noDi[54]}  ${noDi[55]}  ${noDi[56]}  ${noDi[57]}  ${noDi[58]}  ${noDi[59]}  ${noDi[60]}  ${noDi[61]}  ${noDi[62]}`);
+			let maxDepth = Math.floor(Math.log2(this.nodes.length + 1));
+			for(let i = 1; i <= maxDepth; i++) {
+				let lowBound = (2 ** (i - 1)) - 1; // - 1: zero-based index
+				let upBound = (2 ** i) - 1;
+				let toDisplay = " ".repeat((2 ** (maxDepth - i + 1)) - 2);
+				for(let j = lowBound; j < upBound; j++) {
+					toDisplay += noDi[j];
+					toDisplay += " ".repeat((2 ** (maxDepth - i + 2)) - 2);
+				}
+				displayText(toDisplay.trimEnd());
 			}
 			displayText();
 		}
@@ -189,8 +123,18 @@ function day18(input) {
 				newNodes[Math.floor(i / 2) - 1].children.push(newNodes[i - 1]);
 			}
 			this.nodes = newNodes;
-			//displayText(`After merging.`);
-			//this.displayTree();
+		}
+
+		this.dup = function() {
+			let newNodes = [];
+			for(let i = 0; i < this.nodes.length; i++) {
+				newNodes.push(new TreeNode(i + 1));
+				newNodes[i].value = this.nodes[i].value;
+			}
+			for(let i = 2; i < newNodes.length; i++) {
+				newNodes[i - 1].parent = newNodes[Math.floor(i / 2) - 1];
+			}
+			return new Tree(newNodes);
 		}
 
 		this.reduce = function() {
@@ -236,10 +180,9 @@ function day18(input) {
 				// i stands for... IDs
 				explodes(i);
 			}
-			//displayText(`After explosions.`);
-			//this.displayTree();
 
-			const CHECK_ORDER = [16, 8, 17, 4, 18, 9, 19, 2, 20, 10, 21, 5, 22, 11, 23, 1, 24, 12, 25, 6, 26, 13, 27, 3, 28, 14, 29, 7, 30, 15, 31];
+			const CHECK_ORDER = [16, 8, 17, 4, 18, 9, 19, 2, 20, 10, 21, 5, 22, 11, 23, 1, 
+								 24, 12, 25, 6, 26, 13, 27, 3, 28, 14, 29, 7, 30, 15, 31];
 			for(let i = 0; i < CHECK_ORDER.length; i++) {
 				// i stands for... index into CHECK_ORDER
 				if(this.nodes[CHECK_ORDER[i] - 1].value && this.nodes[CHECK_ORDER[i] - 1].value > 9) {
@@ -248,20 +191,15 @@ function day18(input) {
 					this.nodes[CHECK_ORDER[i] - 1].children[1].value = Math.ceil(this.nodes[CHECK_ORDER[i] - 1].value / 2);
 					this.nodes[CHECK_ORDER[i] - 1].value = undefined;
 
-					//displayText(`After a split.`);
-					//this.displayTree();
 					// Then we have to check if it explodes...
 					this.nodes[CHECK_ORDER[i] - 1].children[0].id > 31 ? explodes(this.nodes[CHECK_ORDER[i] - 1].children[0].id) : false;
 					i = -1;
 					// - 1 since i++ happens right after this.
-					//displayText(`After a split's explosion check.`);
-					//this.displayTree();
 				}
 			}
 
 			this.nodes.splice(31);
 			// Delete all nodes from ID 32 to the end.
-
 		}
 	}
 
@@ -309,19 +247,40 @@ function day18(input) {
 		return tree;
 	});
 
+	let numSums = [];
+	for(let k = 0; k < numbers.length; k++) {
+		for(let l = 0; l < numbers.length; l++) {
+			if(k !== l) {
+				let newSum = numbers[k].dup()
+				newSum.merge(numbers[l].dup().nodes);
+				newSum.reduce();
+				numSums.push(calcMag(newSum.nodes[0]));
+			}
+		}
+	}
+	let maxSum = Math.max(...numSums);
+
+	let sumCount = 1;
 	while(numbers.length !== 1) {
 		numbers[0].merge(numbers[1].nodes);
 		numbers[0].reduce();
 		numbers.splice(1, 1);
-		displayText(`~~~~~~~~~~FINAL TREE~~~~~~~~~~`);
+		displayText(`Tree after summing 0 and ${sumCount++}:`);
 		numbers[0].displayTree();
-		//throw `Hold on`;
 	}
 
 	function calcMag(node) {
-		if(node.value || node.value === 0) return node.value;
-		return (3 * calcMag(node.children[0])) + (2 * calcMag(node.children[1]));
+		if(node.value || node.value === 0) {
+			return node.value;
+		}
+		let nodeMag = (3 * calcMag(node.children[0])) + (2 * calcMag(node.children[1]));
+		return nodeMag;
 	}
 
 	displayText(`Final mag: ${calcMag(numbers[0].nodes[0])}`);
+	displayText(`Max sum: ${maxSum}`);
+	updateCaption(`A number of binary trees are displayed.`);
+	updateCaption(`Each one represents the result of summing the first two elements of the list.`);
+	updateCaption(`The magnitude of the last tree is ${calcMag(numbers[0].nodes[0])}.`);
+	updateCaption(`Meanwhile, the max magnitude obtained from summing two elements is ${maxSum}.`);
 }
