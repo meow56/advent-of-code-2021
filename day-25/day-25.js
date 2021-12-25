742bcd616fde8311d6ecd132d001c911b485f811
meow56 <spinattacktotheface@gmail.com>
2021-12-25 09:49:20 -0600

    Day 25-1
    Final day! Sea cucumbers! Display that takes a really long time since it has to write 10 million characters to the screen! Yeah! (I cut it down so it only displays every 10 steps, so it only has to display 1 million characters.)

---
+++ day-25/day-25.js
@@ -0,0 +1,79 @@
+"use strict";
+
+function day25(input) {
+	/*input = `v...>>.vv>
+.vv>>.vv..
+>>.>v>...v
+>>v>>.>.v.
+v>v.vv.v..
+>.>>..v...
+.vv..>.>v.
+v.v..>>v.v
+....v..v.>`;/**/
+	const FILE_REGEX = /(?:\.|>|v)+/gm;
+	let cucumbers = [];
+	let entry;
+	while(entry = FILE_REGEX.exec(input)) {
+		cucumbers.push(entry[0].split(""));
+	}
+
+	let previousResult = "";
+	let stepCount = 1;
+	let display = [];
+	while(previousResult !== cucumbers.flat().join()) {
+		previousResult = cucumbers.flat().join();
+		let nextStep = [];
+		for(let i = 0; i < cucumbers.length; i++) {
+			nextStep.push(new Array(cucumbers[i].length).fill("."));
+		}
+		for(let i = 0; i < cucumbers.length; i++) {
+			for(let j = 0; j < cucumbers[i].length; j++) {
+				if(cucumbers[i][j] === "v") {
+					nextStep[i][j] = cucumbers[i][j];
+					continue;
+				} else if(cucumbers[i][j] === ".") {
+					continue;
+				}
+				let target = [i, (j + 1) % cucumbers[i].length];
+				if(cucumbers[target[0]][target[1]] !== ".") {
+					nextStep[i][j] = ">";
+				} else {
+					nextStep[target[0]][target[1]] = ">";
+					nextStep[i][j] = ".";
+				}
+			}
+		}
+		cucumbers = nextStep;
+		nextStep = [];
+		for(let i = 0; i < cucumbers.length; i++) {
+			nextStep.push(new Array(cucumbers[i].length).fill("."));
+		}
+		for(let i = 0; i < cucumbers.length; i++) {
+			for(let j = 0; j < cucumbers[i].length; j++) {
+				if(cucumbers[i][j] === ">") {
+					nextStep[i][j] = cucumbers[i][j];
+					continue;
+				} else if(cucumbers[i][j] === ".") {
+					continue;
+				}
+				let target = [(i + 1) % cucumbers.length, j];
+				if(cucumbers[target[0]][target[1]] !== ".") {
+					nextStep[i][j] = "v";
+				} else {
+					nextStep[target[0]][target[1]] = cucumbers[i][j];
+					nextStep[i][j] = ".";
+				}
+			}
+		}
+		cucumbers = nextStep;
+		display.push(cucumbers.slice().map(e => e.slice()));
+		//if(stepCount > 60) throw `no.`;
+	}
+
+	for(let j = 0; j < display.length; j += 10) {
+		displayText(`Step ${j + 1}:`);
+		for(let i = 0; i < display[j].length; i++) {
+			displayText(display[j][i].join(""));
+		}
+	}
+}
